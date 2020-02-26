9fb97d8fca471f6747a1150e11a658fe2ca48693
Bodovix <stbbap@gmail.com>
2020-02-26 11:24:01 +0000

    empty parser class added - need to implement

---
+++ CompilersAssesment/CompilersAssesment/PALCompiler/PALParser.cs
@@ -0,0 +1,19 @@
+﻿using AllanMilne.Ardkit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompilersAssesment.PALCompiler
+{
+    internal class PALParser : RecoveringRdParser
+    {
+        public PALParser(IScanner scan) : base(scan)
+        {
+        }
+
+        protected override void recStarter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
