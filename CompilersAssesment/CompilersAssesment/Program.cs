c4d785bb35ef19acc31f77e25ad4041971a3dbff
Bodovix <stbbap@gmail.com>
2020-02-26 11:03:00 +0000

    init

---
+++ CompilersAssesment/CompilersAssesment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompilersAssesment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
