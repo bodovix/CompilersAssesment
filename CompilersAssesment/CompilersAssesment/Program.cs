d4c327c4cc96fcec71bd1415d02af64a647e2218
Bodovix <stbbap@gmail.com>
2020-04-30 14:12:55 +0100

    forgot to output errors in Main method (TTD ;D)

--- CompilersAssesment/CompilersAssesment/Program.cs
+++ CompilersAssesment/CompilersAssesment/Program.cs
@@ -18,6 +18,11 @@ namespace CompilersAssesment
             }
             RunCompiler run = new RunCompiler();
             run.Execute(args[0]);
+
+            //output errors if there are any
+            if (run.ErrorsForTests != null)
+                foreach (string err in run.ErrorsForTests)
+                    Console.WriteLine(err);
         }
     }
 }
